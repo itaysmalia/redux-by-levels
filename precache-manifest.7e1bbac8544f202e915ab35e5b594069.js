576a7c83d061fb912382cd1b26549829e2f08b37
Itay Sin-Malia <itay.sinmalia@brightmachines.com>
2020-10-22 14:17:13 +0300

    Updates

---
+++ precache-manifest.7e1bbac8544f202e915ab35e5b594069.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3de95d379885673d6542513e9fca695b",
+    "url": "/redux-by-levels/index.html"
+  },
+  {
+    "revision": "27182b4bd3d469c7e84d",
+    "url": "/redux-by-levels/static/css/main.3bdfd28b.chunk.css"
+  },
+  {
+    "revision": "7d94ac86b87053d12faf",
+    "url": "/redux-by-levels/static/js/2.778fd382.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/redux-by-levels/static/js/2.778fd382.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27182b4bd3d469c7e84d",
+    "url": "/redux-by-levels/static/js/main.fa9412e1.chunk.js"
+  },
+  {
+    "revision": "ee9273602f68a3b84ea4",
+    "url": "/redux-by-levels/static/js/runtime-main.e6105d5a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/itaysmalia.github.io/redux-by-levels/precache-manifest.09769080a1508742b4e3e055e252cf44.js"
+  "/redux-by-levels/precache-manifest.7e1bbac8544f202e915ab35e5b594069.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/itaysmalia.github.io/redux-by-levels/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/redux-by-levels/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
