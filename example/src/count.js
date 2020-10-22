36c481f8455831059de30945187bb03378b511c9
Itay Sin-Malia <itay.sinmalia@brightmachines.com>
2020-10-22 14:58:06 +0300

    moved example tmp

--- example/src/count.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { useRBLState } from 'redux-by-levels'
-
-function Count() {
-  const [count, setCount] = useRBLState()
-  return <button onClick={() => setCount(count + 1)}>count - {count}</button>
-}
-
-export default Count
--- example/src/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'redux'
-import { getRBLReducer } from 'redux-by-levels'
-
-const initialState = { count: 0, name: 'foo' }
-
-const store = createStore(getRBLReducer(initialState))
-
-export default store
