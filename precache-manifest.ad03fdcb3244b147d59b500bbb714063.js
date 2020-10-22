eb48b7b47e28ab5e4cf6214779e12ea868135ad5
Itay Sin-Malia <itay.sinmalia@brightmachines.com>
2020-10-22 13:56:24 +0300

    Updates

---
+++ precache-manifest.ad03fdcb3244b147d59b500bbb714063.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c74e5690bb0c276bc0da4c667858dbed",
+    "url": "./index.html"
+  },
+  {
+    "revision": "27182b4bd3d469c7e84d",
+    "url": "./static/css/main.3bdfd28b.chunk.css"
+  },
+  {
+    "revision": "7d94ac86b87053d12faf",
+    "url": "./static/js/2.778fd382.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.778fd382.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27182b4bd3d469c7e84d",
+    "url": "./static/js/main.fa9412e1.chunk.js"
+  },
+  {
+    "revision": "ffd412dbc5c9a6f46df4",
+    "url": "./static/js/runtime-main.6383f993.js"
+  }
+]);
